2f5e70cbf4c682dbbbeed6a1ca4cf69101190d66
Ryszard Matula <ryszard.matula.dev@gmail.com>
2023-01-29 19:22:47 +0100

    Protected add-resolution

--- src/pages/add-resolution.tsx
+++ src/pages/add-resolution.tsx
@@ -1,6 +1,27 @@
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
 import { StyledStack } from "../client/components/common";
 import { Page } from "../client/layout/page";
+import { getServerAuthSession } from "../server/auth";
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const session = await getServerAuthSession(ctx);
+
+  if (!session) {
+    return {
+      props: {
+        session,
+      },
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: { session },
+  };
+};
 
 const AddResolution: NextPage = () => {
   return (
